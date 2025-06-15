5d8876243672240eee25f7c236fbbd7a97e74bd5
Meu Nome <meu-email@meu-servidor.com>
2025-06-14 22:01:06 -0300

    Help

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+Console.Clear();
+
+
+//Codigo de organização de alelos
+//Se mexer nisso eu te mato 
+
+string NormalizaAlelo(string alelos)
+{
+    if (alelos.Contains("A") && alelos.Contains("A".ToLower()))
+        return "Aa";
+    else if (alelos == "AA" || alelos == "aa")
+        return alelos;
+    else
+        return new string(alelos.OrderByDescending(c => c).ToArray());
+}
+
+ //Requisitos
+Console.WriteLine("Alelos da Mãe (AA, Aa ou aa): ");
+    String AleloM = NormalizaAlelo(Console.ReadLine()!).Substring(0, 2);
+
+Console.WriteLine("Alelos do Pai (AA, Aa ou aa): ");
+    String AleloP = NormalizaAlelo(Console.ReadLine()!).Substring(0, 2);
+
+Console.WriteLine("Tipo de dominância (C/I): ");
+    String domi = Console.ReadLine()!;
+
+
+ //Separação de Alelos, preferencia para o com A>a
+string AleloM1 = AleloM.Substring(0, 1);
+string AleloM2 = AleloM.Substring(1, 1);
+string AleloP1 = AleloP.Substring(0, 1);
+string AleloP2 = AleloP.Substring(1, 1);
+
+string Junção1 = NormalizaAlelo($"{AleloM1}{AleloP1}");
+string Junção2 = NormalizaAlelo($"{AleloM2}{AleloP1}");
+string Junção3 = NormalizaAlelo($"{AleloM1}{AleloP2}");
+string Junção4 = NormalizaAlelo($"{AleloM2}{AleloP2}");
+
+
+//Tabela>>
+Console.WriteLine(@$"
+
+    | {AleloM1} | {AleloM2} 
+---------------------------------------
+{AleloP1} | {Junção1} | {Junção2}
+---------------------------------------
+{AleloP2} | {Junção3} | {Junção4}
+
+");
+
