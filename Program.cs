679d889d15da7cd9c1cee181e57929bf9eef3dd1
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-06-17 03:39:13 -0300

    Correção de bug gerado após a adição das sub-rotinas

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 Console.Clear();
 
-//Código de organização de alelos
-//Se mexer nisso eu te mato
-//Fiz uma alteração no código a baixo ,porém os alelos parecem da um erro na parte inferior ficando "a aa a" ao invez de "a Aa a", mas na minha cabeça da pra corrigir quando for verificar a Dominancia.
+//Codigo de organização de alelos
+//Se mexer nisso eu te mato 
+
 string NormalizaAlelo(string alelos)
 {
     if (alelos.Contains("A") && alelos.Contains("A".ToLower()))
@@ -14,27 +15,6 @@ string NormalizaAlelo(string alelos)
     else
         return new string(alelos.OrderByDescending(c => c).ToArray());
 }
-
-
-//Requisitos
-    Console.WriteLine("Alelos da Mãe (AA, Aa ou aa): ");
-string aleloM = Console.ReadLine()!;
-
-Console.WriteLine("Alelos do Pai (AA, Aa ou aa): ");
-string aleloP = Console.ReadLine()!;
-
-Console.WriteLine("Tipo de dominância (C/I): ");
-string domi = Console.ReadLine()!;
-
-string aleloM_norm = NormalizaAlelo(aleloM);
-string aleloP_norm = NormalizaAlelo(aleloP);
-
-(string aleloM1, string aleloM2) = SepararAlelos(aleloM_norm);
-(string aleloP1, string aleloP2) = SepararAlelos(aleloP_norm);
-
-string[] juncos = GerarCombinacoes(aleloM1, aleloM2, aleloP1, aleloP2);
-ImprimirTabela(aleloM1, aleloM2, aleloP1, aleloP2, juncos);
-
 //Separação de Alelos, preferência para o com Aa
 //sub-rotina para separação dos alelos - Gustavo (Não parece ter dado problemas após a alteração, na duvida se der b.o pro resto do programa eu tenho o backup no meu pc)
 (string, string) SepararAlelos(string alelos)
@@ -42,22 +22,45 @@ ImprimirTabela(aleloM1, aleloM2, aleloP1, aleloP2, juncos);
     return (alelos.Substring(0, 1), alelos.Substring(1, 1));
 }
 //Sub-rotina GerarCombinações -Gustavo
-string[] GerarCombinacoes(string aleloM1, string aleloM2, string aleloP1, string aleloP2)
+string[] GerarJuncoes(string AleloM1, string AleloM2, string AleloP1, string AleloP2)
 {
     return new string[]
     {
-        NormalizaAlelo(aleloM1[0].ToString() + aleloP1[0].ToString()),
-        NormalizaAlelo(aleloM1[0].ToString() + aleloP2[0].ToString()),
-        NormalizaAlelo(aleloM2[0].ToString() + aleloP1[0].ToString()),
-        NormalizaAlelo(aleloM2[0].ToString() + aleloP2[0].ToString())
+        NormalizaAlelo($"{AleloM1}{AleloP1}"),
+        NormalizaAlelo($"{AleloM2}{AleloP1}"),
+        NormalizaAlelo($"{AleloM1}{AleloP2}"),
+        NormalizaAlelo($"{AleloM2}{AleloP2}")
     };
 }
 
-//Sub-rotina tabela -Gustavo
-void ImprimirTabela(string aleloM1, string aleloM2, string aleloP1, string aleloP2, string[] juncoes)
+void ImprimirTabela(string AleloM1, string AleloM2, string AleloP1, string AleloP2, string[] Juncoes)
 {
-    Console.WriteLine($"\n      | {aleloP1} | {aleloP2} |");
-    Console.WriteLine("-------------------");
-    Console.WriteLine($"  {aleloM1}  | {juncoes[0]} | {juncoes[1]} ");
-    Console.WriteLine($"  {aleloM2}  | {juncoes[2]} | {juncoes[3]} ");
+    Console.WriteLine("\n--- Genética Mendeliana ---\n");
+    Console.WriteLine($"  | {AleloM1} | {AleloM2}");
+    Console.WriteLine("--------------");
+    Console.WriteLine($"{AleloP1} | {Juncoes[0]} | {Juncoes[1]}");
+    Console.WriteLine("--------------");
+    Console.WriteLine($"{AleloP2} | {Juncoes[2]} | {Juncoes[3]}");
 }
+
+
+//Requisitos
+Console.WriteLine("Alelos da Mãe (AA, Aa ou aa): ");
+string AleloM = Console.ReadLine()!;
+Console.WriteLine("Alelos do Pai (AA, Aa ou aa): ");
+string AleloP = Console.ReadLine()!;
+Console.WriteLine("Tipo de dominância (C/I): ");
+string domi = Console.ReadLine()!;
+
+//Separação e normalização
+string AleloM_norm = NormalizaAlelo(AleloM);
+string AleloP_norm = NormalizaAlelo(AleloP);
+
+string AleloM1, AleloM2;
+string AleloP1, AleloP2;
+(AleloM1, AleloM2) = SepararAlelos(AleloM_norm);
+(AleloP1, AleloP2) = SepararAlelos(AleloP_norm);
+
+//Combinação e saída
+string[] Juncoes = GerarJuncoes(AleloM1, AleloM2, AleloP1, AleloP2);
+ImprimirTabela(AleloM1, AleloM2, AleloP1, AleloP2, Juncoes);
