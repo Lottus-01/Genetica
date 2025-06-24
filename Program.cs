08962ddc2f750f299abcfc5e584a7f31f1d2aba1
Vittoria.Lopes <lopes.tt06@gmail.com>
2025-06-24 19:57:25 -0300

    Adicionado o calculo dos fenótipos

--- Program.cs
+++ Program.cs
@@ -8,20 +8,19 @@ Console.Clear();
 
 string NormalizaAlelo(string alelos)
 {
-    if (alelos.Contains("A") && alelos.Contains("A".ToLower()))
+    if (alelos.Contains("A") && alelos.Contains("a"))
         return "Aa";
     else if (alelos == "AA" || alelos == "aa")
         return alelos;
     else
         return new string(alelos.OrderByDescending(c => c).ToArray());
 }
-//Separação de Alelos, preferência para o com Aa
-//sub-rotina para separação dos alelos - Gustavo (Não parece ter dado problemas após a alteração, na duvida se der b.o pro resto do programa eu tenho o backup no meu pc)
+//Separação de Alelos
 (string, string) SepararAlelos(string alelos)
 {
     return (alelos.Substring(0, 1), alelos.Substring(1, 1));
 }
-//Sub-rotina GerarCombinações -Gustavo
+//Gerar combinações
 string[] GerarJuncoes(string AleloM1, string AleloM2, string AleloP1, string AleloP2)
 {
     return new string[]
@@ -43,6 +42,23 @@ void ImprimirTabela(string AleloM1, string AleloM2, string AleloP1, string Alelo
     Console.WriteLine($"{AleloP2} | {Juncoes[2]} | {Juncoes[3]}");
 }
 
+// Interpretar fenótipo baseado na dominância
+string InterpretarFenotipo(string genotipo, string tipoDominancia)
+{
+    return tipoDominancia.ToUpper() switch
+    {
+        "C" => genotipo.Contains('A')
+                ? "Não apresenta a característica recessiva"
+                : "Apresenta a característica recessiva",
+        "I" => genotipo switch
+        {
+            "AA" => "Fenótipo dominante",
+            "aa" => "Fenótipo recessivo",
+            _ => "Fenótipo intermediário (mistura)"
+        },
+        _ => "Tipo de dominância inválido"
+    };
+}
 
 //Requisitos
 Console.WriteLine("Alelos da Mãe (AA, Aa ou aa): ");
@@ -61,6 +77,25 @@ string AleloP1, AleloP2;
 (AleloM1, AleloM2) = SepararAlelos(AleloM_norm);
 (AleloP1, AleloP2) = SepararAlelos(AleloP_norm);
 
-//Combinação e saída
+//Combinação
 string[] Juncoes = GerarJuncoes(AleloM1, AleloM2, AleloP1, AleloP2);
+
+// Contar frequências dos genótipos gerados
+var contagem = Juncoes
+    .GroupBy(j => j)
+    .ToDictionary(g => g.Key, g => g.Count());
+
+// Calcular total
+int total = Juncoes.Length;
+
+Console.WriteLine("\nFrequência dos genótipos e fenótipos:");
+Console.WriteLine("-------------------------------------");
+
+foreach (var item in contagem)
+{
+    double porcentagem = (item.Value / (double)total) * 100;
+    string fenotipo = InterpretarFenotipo(item.Key, domi);
+    Console.WriteLine($"{item.Key}: {porcentagem:0}% - {fenotipo}");
+}
+
 ImprimirTabela(AleloM1, AleloM2, AleloP1, AleloP2, Juncoes);
